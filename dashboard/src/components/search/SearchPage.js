f277b0eff0d9e501f3a6c5b067438163e0c54d27
Trevor Spreadbury <trevorspreadbury@gmail.com>
2022-03-23 20:28:55 -0500

    add documentation to searchpage

--- dashboard/src/components/search/SearchPage.js
+++ dashboard/src/components/search/SearchPage.js
@@ -1,5 +1,8 @@
 import React from 'react'
-
+// This page comes second in importance. The main idea is
+// to create an html form to mimic an exsclaim query json
+// to send a post request to the api that will initiate
+// a run of the pipeline
 
 const SearchPage = () => {
     return (
